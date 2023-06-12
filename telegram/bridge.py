1a90e6741a7893da5848dc7cea8ad0c0503c4922
hcn1z1 <mohamedevander@gmail.com>
2023-06-12 20:30:17 +0200

    server update

--- telegram/bridge.py
+++ telegram/bridge.py
@@ -12,19 +12,19 @@ def communicate(chatid,information:str):
 
 @bot.message_handler()
 def shortner(message):
-    api = "https://vvath.com/api/shortner"
+    api = "https://vs-s.link/api/shortner"
     url = message.text
-    if "vvath" in url:
+    if "vs-s" in url:
         try:
             vvath,newLink = url.split(";")
             data = {"url":newLink,"shortner":vvath[len(vvath)-6:],"telegram":message.chat.id}
             response = requests.post(api,json=data)
             if response.status_code == 500 : bot.send_message(message.chat.id,"not a valide link !\nexample of valide link : https://example.com")
-            else : bot.send_message(message.chat.id,"https://vvath.com/"+response.text)
+            else : bot.send_message(message.chat.id,"https://vs-s.link/"+response.text)
         except:
             bot.send_message(message.chat.id,"Bad Link")
     else:
         response = requests.post(api,json={"url":url,"telegram":message.chat.id})
         if response.status_code == 500 : bot.send_message(message.chat.id,"not a valide link !\nexample of valide link : https://example.com")
-        else : bot.send_message(message.chat.id,"https://vvath.com/"+response.text)
+        else : bot.send_message(message.chat.id,"https://vs-s.link/"+response.text)
 
